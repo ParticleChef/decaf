647b33cdc84931bdc1d0175c783adf832664a52f
ParticleChef <demonichje@gmail.com>
2024-07-06 19:45:23 +0900

    split runner version 2

--- analysis/checkEvents/split_and_run2.py
+++ analysis/checkEvents/split_and_run2.py
@@ -13,7 +13,7 @@ def split_file(input_file, lines_per_file):
     file_count = (total_lines // lines_per_file) + (1 if total_lines % lines_per_file !=0 else 0)
 
     for i in range(file_count):
-        with open(f'split_inputs/inputrootlist_part{i}.txt', 'w') as f_part:
+        with open(f'split_inputs2/inputrootlist_part{i}.txt', 'w') as f_part:
             for line in lines[i*lines_per_file:(i+1)*lines_per_file]:
                 f_part.write(line)
     return file_count
@@ -24,18 +24,19 @@ def run_macro(input_file, output_file, macro_file):
     subprocess.run(cmd)
 
 if __name__ == "__main__":
+    #input_file = 'EGM18Dlist.txt'
     input_file = 'inputrootlist.txt'
-    lines_per_file = 5
+    lines_per_file = 150
     macro_file = 'test2.C'
 
-    output_dir = 'result_files'
+    output_dir = 'result_files2'
     os.makedirs(output_dir, exist_ok=True)
 
     start_time = time.time()
 
     file_count = split_file(input_file, lines_per_file)
 
-    input_file_list = [f'split_inputs/inputrootlist_part{i}.txt' for i in range(file_count)]
+    input_file_list = [f'split_inputs2/inputrootlist_part{i}.txt' for i in range(file_count)]
 
 ## Method 1
 #    run_macro(input_file_list, macro_file, output_prefix)
