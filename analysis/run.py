3f2d21f0eaaadbab370e0138099fb96b6733970b
ParticleChef <demonichje@gmail.com>
2026-01-06 21:47:57 +0900

    Update custom Nanoaod scheme

--- analysis/run.py
+++ analysis/run.py
@@ -12,7 +12,7 @@ import uproot
 import numpy as np
 from coffea import processor
 from coffea.util import load, save
-from libs.mycoffea import CustomNanoAODSchema, AK15SubJet, AK15Jet
+from libs.mycoffeav2 import CustomNanoAODSchema, AK15SubJet, AK15Jet
 
 import warnings
 warnings.filterwarnings("ignore")
