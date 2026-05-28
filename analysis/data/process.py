ab3ba9c1625785b52507c87f7b892d4ee7da3b9f
ParticleChef <demonichje@gmail.com>
2026-05-28 18:47:18 +0900

    Minor update: notebook, btag files, input files etc.

--- analysis/data/process.py
+++ analysis/data/process.py
@@ -3,11 +3,12 @@
 processes = {
 
     ## Data
-    'JetMET':('JetMET','Data',1),
-    'EGamma':('EGamma','Data',1),
-    'Muon':('Muon','Data',1),
-    'Muon0':('Muon0','Data',1),
-    'Muon1':('Muon1','Data',1),
+    'JetMET':('JetMET','Data',-1),
+    'EGamma':('EGamma','Data',-1),
+    'Muon':('Muon','Data',-1),
+    'Muon0':('Muon0','Data',-1),
+    'Muon1':('Muon1','Data',-1),
+
 
     ## DYto2L-2Jets
     'DYto2L-2Jets_MLL-50_PTLL-100to200_1J_TuneCP5_13p6TeV_amcatnloFXFX-pythia8': ('DYto2L-2Jets_MLL-50_PTLL-100to200_1J', 'MC', 45.42),
@@ -22,11 +23,11 @@ processes = {
     'DYto2L-2Jets_MLL-50_PTLL-600_2J_TuneCP5_13p6TeV_amcatnloFXFX-pythia8': ('DYto2L-2Jets_MLL-50_PTLL-600_2J', 'MC', 0.07019),
 
     ## DYto2L-4Jets
-    #'DYto2L-4Jets_MLL-50_PTLL-100to200_TuneCP5_13p6TeV_madgraphMLM-pythia8': ('DYto2L-4Jets_MLL-50_PTLL-100to200', 'MC', 58.46),
-    #'DYto2L-4Jets_MLL-50_PTLL-200to400_TuneCP5_13p6TeV_madgraphMLM-pythia8': ('DYto2L-4Jets_MLL-50_PTLL-200to400', 'MC', 6.678),
-    #'DYto2L-4Jets_MLL-50_PTLL-400to600_TuneCP5_13p6TeV_madgraphMLM-pythia8': ('DYto2L-4Jets_MLL-50_PTLL-400to600', 'MC', 0.3833),
-    #'DYto2L-4Jets_MLL-50_PTLL-40to100_TuneCP5_13p6TeV_madgraphMLM-pythia8': ('DYto2L-4Jets_MLL-50_PTLL-40to100', 'MC', 403.7),
-    #'DYto2L-4Jets_MLL-50_PTLL-600_TuneCP5_13p6TeV_madgraphMLM-pythia8': ('DYto2L-4Jets_MLL-50_PTLL-600', 'MC', 0.06843),
+    'DYto2L-4Jets_MLL-50_PTLL-100to200_TuneCP5_13p6TeV_madgraphMLM-pythia8': ('DYto2L-4Jets_MLL-50_PTLL-100to200', 'MC', 58.46),
+    'DYto2L-4Jets_MLL-50_PTLL-200to400_TuneCP5_13p6TeV_madgraphMLM-pythia8': ('DYto2L-4Jets_MLL-50_PTLL-200to400', 'MC', 6.678),
+    'DYto2L-4Jets_MLL-50_PTLL-400to600_TuneCP5_13p6TeV_madgraphMLM-pythia8': ('DYto2L-4Jets_MLL-50_PTLL-400to600', 'MC', 0.3833),
+    'DYto2L-4Jets_MLL-50_PTLL-40to100_TuneCP5_13p6TeV_madgraphMLM-pythia8': ('DYto2L-4Jets_MLL-50_PTLL-40to100', 'MC', 403.7),
+    'DYto2L-4Jets_MLL-50_PTLL-600_TuneCP5_13p6TeV_madgraphMLM-pythia8': ('DYto2L-4Jets_MLL-50_PTLL-600', 'MC', 0.06843),
 
     ## GJ
     'GJ_PTG-100to200_TuneCP5_13p6TeV_amcatnlo-pythia8': ('GJ_PTG-100to200', 'MC', 1396.0),
@@ -48,26 +49,26 @@ processes = {
     'QCD_PT-800to1000_TuneCP5_13p6TeV_pythia8': ('QCD_PT-800to1000', 'MC', 30.57),
     'QCD_PT-80to120_TuneCP5_13p6TeV_pythia8': ('QCD_PT-80to120', 'MC', 2534000.0),
 
-    ## TbarBtoLminusNuB-s-channel-4FS
-    'TbarBtoLminusNuB-s-channel-4FS_TuneCP5_13p6TeV_amcatnlo-pythia8': ('TbarBtoLminusNuB-s-channel-4FS', 'MC', 1.43),
+    ## ST-TbarBtoLminusNuB-s-channel-4FS
+    'ST-TbarBtoLminusNuB-s-channel-4FS_TuneCP5_13p6TeV_amcatnlo-pythia8': ('ST-TbarBtoLminusNuB-s-channel-4FS', 'MC', 1.43),
 
-    ## TbarWplusto2L2Nu
-    'TbarWplusto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8': ('TbarWplusto2L2Nu', 'MC', 3.9735),
+    ## ST-TbarWplusto2L2Nu
+    'ST-TbarWplusto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8': ('ST-TbarWplusto2L2Nu', 'MC', 3.9735),
 
-    ## TbarWplusto4Q
-    'TbarWplusto4Q_TuneCP5_13p6TeV_powheg-pythia8': ('TbarWplusto4Q', 'MC', 15.942),
+    ## ST-TbarWplusto4Q
+    'ST-TbarWplusto4Q_TuneCP5_13p6TeV_powheg-pythia8': ('ST-TbarWplusto4Q', 'MC', 15.942),
 
-    ## TbarWplustoLNu2Q
-    'TbarWplustoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8': ('TbarWplustoLNu2Q', 'MC', 15.918),
+    ## ST-TbarWplustoLNu2Q
+    'ST-TbarWplustoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8': ('ST-TbarWplustoLNu2Q', 'MC', 15.918),
 
-    ## TBbartoLplusNuBbar-s-channel-4FS
-    'TBbartoLplusNuBbar-s-channel-4FS_TuneCP5_13p6TeV_amcatnlo-pythia8': ('TBbartoLplusNuBbar-s-channel-4FS', 'MC', 2.278),
+    ## ST-TBbartoLplusNuBbar-s-channel-4FS
+    'ST-TBbartoLplusNuBbar-s-channel-4FS_TuneCP5_13p6TeV_amcatnlo-pythia8': ('ST-TBbartoLplusNuBbar-s-channel-4FS', 'MC', 2.278),
 
-    ## TQbarto2Q-t-channel
-    'TQbarto2Q-t-channel_TuneCP5_13p6TeV_powheg-pythia8': ('TQbarto2Q-t-channel', 'MC', 43.16),
+    ## ST-TQbarto2Q-t-channel
+    'ST-TQbarto2Q-t-channel_TuneCP5_13p6TeV_powheg-pythia8': ('ST-TQbarto2Q-t-channel', 'MC', 43.16),
 
-    ## TQbartoLNu-t-channel
-    'TQbartoLNu-t-channel_TuneCP5_13p6TeV_powheg-pythia8': ('TQbartoLNu-t-channel', 'MC', 86.45),
+    ## ST-TQbartoLNu-t-channel
+    'ST-TQbartoLNu-t-channel_TuneCP5_13p6TeV_powheg-pythia8': ('ST-TQbartoLNu-t-channel', 'MC', 86.45),
 
     ## TTto2L2Nu
     'TTto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8': ('TTto2L2Nu', 'MC', 101.802),
@@ -78,14 +79,14 @@ processes = {
     ## TTtoLNu2Q
     'TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8': ('TTtoLNu2Q', 'MC', 407.824),
 
-    ## TWminusto2L2Nu
-    'TWminusto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8': ('TWminusto2L2Nu', 'MC', 3.966),
+    ## ST-TWminusto2L2Nu
+    'ST-TWminusto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8': ('ST-TWminusto2L2Nu', 'MC', 3.966),
 
-    ## TWminusto4Q
-    'TWminusto4Q_TuneCP5_13p6TeV_powheg-pythia8': ('TWminusto4Q', 'MC', 15.915),
+    ## ST-TWminusto4Q
+    'ST-TWminusto4Q_TuneCP5_13p6TeV_powheg-pythia8': ('ST-TWminusto4Q', 'MC', 15.915),
 
-    ## TWminustoLNu2Q
-    'TWminustoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8': ('TWminustoLNu2Q', 'MC', 15.891),
+    ## ST-TWminustoLNu2Q
+    'ST-TWminustoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8': ('ST-TWminustoLNu2Q', 'MC', 15.891),
 
     ## WtoLNu-2Jets
     'WtoLNu-2Jets_0J_TuneCP5_13p6TeV_amcatnloFXFX-pythia8': ('WtoLNu-2Jets_0J', 'MC', 55760.0),
